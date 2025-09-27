273fa9d5c8e2bf9a020bddc45f35183497768167
s8lkkkkk <caydensf21@gmail.com>
2025-09-28 12:23:30 +1300

    Delete utils directory

--- utils/maskToken.js
+++
@@ -1,8 +0,0 @@
-export function maskToken(token, left = 4, right = 4) {
-  if (!token || typeof token !== 'string') return '';
-  if (token.length <= left + right + 4) return token.replace(/./g, '•');
-  const start = token.slice(0, left);
-  const end = token.slice(-right);
-  const middle = '•'.repeat(Math.max(6, token.length - left - right));
-  return `${start}${middle}${end}`;
-}
